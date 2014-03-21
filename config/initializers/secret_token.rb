e0a45b185050cc9b8efccfe3eea2aaa97c91825c
Angie Gove <ang.gove@gmail.com>
2014-03-21 21:03:56 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minijobs::Application.config.secret_token = '2290b17b9c634ae8bf05000b089562f7998973672e6517a889e9b26a83a6a4912d74d3b8c64c931d013b5bf81f6e038d97aec974504863cedc7eb84e803a9eb3'
