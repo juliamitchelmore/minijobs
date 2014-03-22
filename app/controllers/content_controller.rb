6548d788fb0a6a9a7682182f39d6a0edec4b7cf3
Lucy Bain <lucybain@gmail.com>
2014-03-22 11:17:08 +1100

    trying to get profile to be a nav link

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,11 +1,16 @@
 class ContentController < ApplicationController
-  before_filter :authenticate_user!, only: [:dashboard]
+  before_filter :authenticate_user!, only: [:dashboard, :profile]
   def index
   end
 
   def dashboard
     render 'kid/dashboard' and return if current_user.is_a? Kid
     render 'adult/parent_dashboard' and return if current_user.parent?
-    render 'adult/lister_dashboard' and return if current_user.adult?
+    render 'adult/lister_dashboard' and return if current_user.lister?
+  end
+
+  def profile
+    redirect_to kid_path(current_user) and return if current_user.is_a? Kid
+    render adult_path(current_user) and return
   end
 end
