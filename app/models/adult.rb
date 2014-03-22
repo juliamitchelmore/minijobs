b88ac21fa01af4e4c617e8651ddab852de692023
Lucy Bain <lucybain@gmail.com>
2014-03-22 13:23:11 +1100

    slightly easier jobs index page

--- app/models/adult.rb
+++ app/models/adult.rb
@@ -1,6 +1,6 @@
 class Adult < ActiveRecord::Base
-  scope :all_parents, -> { where(type: 'parent') }
-  scope :all_listers, -> { where(type: 'lister') }
+  scope :all_parents, -> { where(role: 'parent') }
+  scope :all_listers, -> { where(role: 'lister') }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,17 +10,21 @@ class Adult < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :address, :email, :latitude, :longitude, :name
 
-  has_many :jobs
+  has_and_belongs_to_many :jobs
   has_many :kids
 
   geocoded_by :address   # can also be an IP address
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 
   def parent?
-    type == 'parent'
+    role == 'parent'
   end
 
   def adult?
-    type == 'adult'
+    role == 'adult'
+  end
+
+  def kid
+    kids.first
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140322014008) do
+ActiveRecord::Schema.define(:version => 20140322020908) do
 
   create_table "adults", :force => true do |t|
     t.string   "name"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(:version => 20140322014008) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "type"
+    t.string   "role"
   end
 
   add_index "adults", ["email"], :name => "index_adults_on_email", :unique => true
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20140322014008) do
     t.string   "name"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.integer  "parent_id"
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
