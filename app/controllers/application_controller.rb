276966fdccec459d8d5e13220cd8db6e9dc3871b
Lucy Bain <lucybain@gmail.com>
2014-03-22 11:28:55 +1100

    gah, the hacks! it burns!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_user
+    current_kid || current_adult
+  end
+
+  def authenticate_user!
+    authenticate_kid! || authenticate_adult!
+  end
 end
